f24bc67b4c1b34861c0bd8332f3cf7f4dd152d89
Nrlsyhmina <nrlsyhmina@gmail.com>
2022-02-02 23:14:29 +0800

    first commit

---
+++ connection/connection.php
@@ -0,0 +1,15 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "mytechclub";
+
+$conn = mysqli_connect($host, $user, $pass, $dbname);
+
+/*if ($conn)
+    echo "connection ok";
+else
+   echo "connection tak ok";*/
+
+
+?>
